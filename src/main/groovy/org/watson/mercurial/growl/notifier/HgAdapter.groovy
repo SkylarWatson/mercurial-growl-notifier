513406d05e60004d4c96f9e819cfe9c799ff2af3
SkylarWatson <skylarrwatson@gmail.com>
2013-06-05 18:37:27 -0500

    Repository location is constant in HgAdapter for now.

--- src/main/groovy/org/watson/mercurial/growl/notifier/HgAdapter.groovy
+++ src/main/groovy/org/watson/mercurial/growl/notifier/HgAdapter.groovy
@@ -1,7 +1,9 @@
 package org.watson.mercurial.growl.notifier;
 
 class HgAdapter {
+    public static final String REPOSITORY_LOCATION
+
     String execute() {
-        'cmd /c "cd C:\\RAD8WS && hg incoming -v"'.execute().text
+        'cmd /c "cd ' + REPOSITORY_LOCATION + ' && hg incoming -v"'.execute().text
     }
 }
